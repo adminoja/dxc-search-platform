655c83f586c81c5269c6e1e7bbfe47f758af7824
kittivong <kittivong@gmail.com>
2025-10-18 10:34:32 +0700

    fix test error using test profile

--- src/test/java/th/go/dxc/platform/search/DxcSearchPlatformApplicationTests.java
+++ src/test/java/th/go/dxc/platform/search/DxcSearchPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package th.go.dxc.platform.search;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DxcSearchPlatformApplicationTests {
 
 	@Test
